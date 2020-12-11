5eb7a625b68d72f12428fa7fcd1e87a8d7433e86
smolnreadytobrawl <noelani.villanueva@gmail.com>
2020-12-10 16:14:03 -0800

    learning how to use dynamic routes in sinatra

--- app.rb
+++ app.rb
@@ -2,5 +2,47 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do 
+    @user_name=params[:name]
+    @user_name.reverse
+  end 
+
+  get '/square/:number' do 
+    @number=params[:number].to_i
+    (@number**2).to_s
+  end 
+
+  get '/say/:number/:phrase' do 
+    str = ""
+    @number=params[:number].to_i
+    @phrase=params[:phrase]
+    @number.times {str += "#{@phrase}"}.to_s
+    str
+  end 
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    @word1=params[:word1]
+    @word2=params[:word2]
+    @word3=params[:word3]
+    @word4=params[:word4]
+    @word5=params[:word5]
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+    
+  end 
+
+  get '/:operation/:number1/:number2' do 
+    @num1=params[:number1].to_i
+    @num2=params[:number2].to_i
+    case params[:operation]
+    when "add"
+      return "#{@num1 + @num2}"
+    when "subtract"
+      return "#{@num1 - @num2}"
+    when "multiply"
+      return "#{@num1 * @num2}"
+    when "divide"
+      return "#{@num1/ @num2}"
+    end
+  end 
 
 end
